39ba2c44b1e17b5b7ca8c6e48b1f707d2b5393ad
unknown <alejandrojzeballos@gmail.com>
2020-12-06 00:50:47 -0400

    server con express ejecutandose

--- AppNotesMongo/src/index.js
+++ AppNotesMongo/src/index.js
@@ -1,1 +1,6 @@
 /* principal */
+const app = require('./server');
+
+app.listen(3000, ()=>{
+    console.log('Server on port 3000');  
+})
--- AppNotesMongo/src/server.js
+++ AppNotesMongo/src/server.js
@@ -1,1 +1,6 @@
 /* codigo express */
+const express = require('express');
+
+const app = express();
+
+module.exports = app;

e9bba56a6ba807ed96f0938a2ad1da2124eca3c0
unknown <alejandrojzeballos@gmail.com>
2020-12-05 23:50:23 -0400

    añadiendo librerias

--- apuntes/apuntes.js
+++ apuntes/apuntes.js
@@ -1,5 +1,16 @@
 /* 
 Usaremos NOdejs y mongodb de maneraLocal
 
+connect-flash guarda y muestra mensajes desde el navegador.
+bcryptjs cifrado de texto.
+express-handlebars crear vistas, hvs podemos crear html, express-session memoria del servidor
+method-override enviar peticiones put o delete con solo js no podemos enviar put y no delete.
+mongoose ORM DE mongoose. edfinir esquemas con los datos.
+passport un modulo para autenticar al usuario. con bcryptjs ademas usar de terceros.
+passport-local.
+
+npm i  express-handlebars@3.1.0 express-session@1.17.0 bcryptjs@2.4.3 mongoose@5.8.11 express@4.17.1 connect-flash@0.1.1 passport@0.4.1 passport-local@1.0.0 method-override@3.0.0
+
+librerias instaladas.
 
 */

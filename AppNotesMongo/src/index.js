1755cdc03ded0becb7076c22a567de25cacac457
unknown <alejandrojzeballos@gmail.com>
2020-12-06 00:56:37 -0400

    confugurando varialbrs de entorno

--- AppNotesMongo/src/index.js
+++ AppNotesMongo/src/index.js
@@ -1,6 +1,6 @@
 /* principal */
 const app = require('./server');
 
-app.listen(3000, ()=>{
-    console.log('Server on port 3000');  
+app.listen(app.get('port'), ()=>{
+    console.log('Server on port', app.get('port'));  
 })
--- AppNotesMongo/src/server.js
+++ AppNotesMongo/src/server.js
@@ -1,6 +1,19 @@
 /* codigo express */
 const express = require('express');
-
+//initializations
 const app = express();
+
+//settings  configuras las dependencias de express
+app.set('port', process.env.PORT || 4000)
+//middlewares funcionies previas
+
+//global variables
+
+
+//routes
+
+//static files
+
+
 
 module.exports = app;

4a6b44464f19a7e32f1353169d3615d38e37f6c3
unknown <alejandrojzeballos@gmail.com>
2020-12-07 23:56:03 -0400

    añadiendo la carpeta views con path

--- AppNotesMongo/src/server.js
+++ AppNotesMongo/src/server.js
@@ -1,10 +1,12 @@
 /* codigo express */
 const express = require('express');
+const path = require('path');
 //initializations
 const app = express();
 
 //settings  configuras las dependencias de express
-app.set('port', process.env.PORT || 4000)
+app.set('port', process.env.PORT || 4000);
+app.set('views',path.join(__dirname, 'views'));
 //middlewares funcionies previas
 
 //global variables

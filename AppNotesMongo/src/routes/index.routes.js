389f399dca823f84b24451e695bb34cda4269aba
unknown <alejandrojzeballos@gmail.com>
2020-12-10 22:58:59 -0400

    añadiendo la ruta about

--- AppNotesMongo/src/routes/index.routes.js
+++ AppNotesMongo/src/routes/index.routes.js
@@ -4,4 +4,9 @@ const router = Router();
 router.get('/',(req,res) => {
     res.render('index');
 });
+
+router.get('/about',(req,res) => {
+    res.render('about');
+});
+
 module.exports = router;

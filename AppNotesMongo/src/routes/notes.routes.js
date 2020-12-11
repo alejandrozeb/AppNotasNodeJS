2440a00edd1642d042ad0ce48370605298d585fe
unknown <alejandrojzeballos@gmail.com>
2020-12-11 01:00:22 -0400

    añadiendo la configuracion para recibir por post

--- AppNotesMongo/src/routes/notes.routes.js
+++ AppNotesMongo/src/routes/notes.routes.js
@@ -1,6 +1,12 @@
 const {Router} = require('express');
 const router = Router()
-const {renderNoteForm} = require('../controllers/notes.controller');
-
+const {renderNoteForm,createNewNote} = require('../controllers/notes.controller');
+//new note
 router.get('/notes/add',renderNoteForm);//mostraremos un formulario
+
+router.post('/notes/add',createNewNote);//cuando se encie datos ni existira comflictos por que tienen diferente verbo(get,post)
+
+//get all note
+
+
 module.exports= router;

024f041206d34b4d64484f66c3ced5a818b05057
unknown <alejandrojzeballos@gmail.com>
2020-12-16 01:42:01 -0400

    verificando session con passport

--- AppNotesMongo/src/config/passport.js
+++ AppNotesMongo/src/config/passport.js
@@ -16,7 +16,26 @@ passport.use(new localStrategy({
         return done(null,false, {message: 'Not user Found'});
     }else{
         //match password's user
-         await user.matchPassword(passport);
+        const match= await user.matchPassword(passport);
+        if(match){
+            return done(null,user);
+            //gurada el usuario en la session
+        }else{
+            return done(null, false, {message: 'Incorrect Password'});
+        }
+        //podemos añadir mas cosas como verificar que sea un correo
     }
 }
 ));
+
+passport.serializeUser((user,done)=>{
+    done(null,user.id);
+} );
+//guarda en la session
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id, (err,user)=>{
+        done(err,user);
+    });
+});
+//cuando navega por las paginas se verifica si esta autorizado
